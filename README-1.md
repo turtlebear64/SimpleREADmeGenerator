3231bd9ff092610cdfcadaccef6fc2709f462e68
Nicolas Danciu <nixrom1@gmail.com>
2022-02-16 19:22:03 -0500

    Adding my ReadmeGenerator

---
+++ README-1.md
@@ -0,0 +1,52 @@
+# READMe-Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+### Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Testing](#testing)
+- [Questions](#questions)
+- [License](#license)
+- [Application Image](#application-image)
+
+## Description
+
+Simple READMe Generator
+
+## Installation
+
+undefined
+
+## Usage
+
+N/a
+
+## Contributing
+
+Any
+
+## Questions
+
+If you have any questions feel free to contact me here:
+
+ ##### Github: [github.com/turtlebear64](https://github.com/turtlebear64)
+
+ ##### Email: [nixrom1@gmail.com](mailto:nixrom1@gmail.com?subject=[GitHub])
+
+## Testing
+
+[Inquirer]
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+You have the freedom to do as you like with this permissive software, as long as an original copy and license notice is included. I cannon be held liable for this software.
+
+## Application Image
+
+ ![Image of Application](N/A)
---
+++ index.js
@@ -0,0 +1,107 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+let path = './README.md';
+let $template = "";
+
+try {
+  if (fs.existsSync(path)) {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Overwrite existing README.md?",
+          name: "overwrite",
+          choices: ["yes", "no", "cancel"]
+        }])
+      .then(function (response) {
+        if (response.overwrite === "no") {
+          path = './README-1.md';
+          getInfo();
+        } else if (response.overwrite === "yes") {
+          getInfo();
+        };
+      })
+  } else { getInfo(); }
+} catch (err) {
+  console.error(err);
+}
+function getInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "Enter a description for your project.",
+        name: "description"
+      },
+      {
+        type: "input",
+        message: "Enter usage information.",
+        name: "usage"
+      },
+      {
+        type: "input",
+        message: "How can others contribute?",
+        name: "contribute"
+      },
+      {
+        type: "input",
+        message: "How can this code be tested?",
+        name: "test"
+      },
+      {
+        type: "list",
+        message: "What licence is relevant to this project?",
+        name: "licence",
+        choices: ["MIT", "Apache", "GPLv3"]
+      },
+      {
+        type: "input",
+        message: "what is your Github username?",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "what is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "Enter your image url (https://github.com/your-repository/...)",
+        name: "image"
+      }
+    ])
+    .then(function (response) {
+      let licenceInfo = "";
+      $template += `# ${response.name}\n\n`;
+      if (response.licence === "MIT") {
+        $template += `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n`;
+        licenceInfo = '(https://opensource.org/licenses/MIT)\n\nYou have the freedom to do as you like with this permissive software, as long as an original copy and license notice is included. I cannon be held liable for this software.\n\n';
+      } else if (response.licence === "Apache") {
+        $template += `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n`;
+        licenceInfo = '(http://www.apache.org/licenses/LICENSE-2.0.html)\n\nYou have the freedom to do as you like with this permissive software. This license also contains a patent license from the contributors of the code.\n\n';
+      } else if (response.licence === "GPLv3") {
+        $template += `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n`;
+        licenceInfo = '(http://www.gnu.org/licenses/gpl-3.0.html)\n\nYou have the freedom to run, study, share, and modify this permissive software. Anyone who acquires this software must make it available to anyone else under the same licensing agreement.\n\n';
+      };      
+      $template += `### Table of Contents\n\n- [Description](#description)\n- [Installation](#installation)\n- [Usage](#usage)\n- [Contributing](#contributing)\n- [Testing](#testing)\n- [Questions](#questions)\n- [License](#license)\n- [Application Image](#application-image)\n\n`;
+      $template += `## Description\n\n${response.description}\n\n`;
+      $template += `## Installation\n\n${response.install}\n\n`;
+      $template += `## Usage\n\n${response.usage}\n\n`;
+      $template += `## Contributing\n\n${response.contribute}\n\n`;
+      $template += `## Questions\n\nIf you have any questions feel free to contact me here:\n\n ##### Github: [github.com/${response.username}](https://github.com/${response.username})\n\n ##### Email: [${response.email}](mailto:${response.email}?subject=[GitHub])\n\n`;
+      $template += `## Testing\n\n[${response.test}]\n\n`;
+      $template += `## License\n\n[${response.licence}]${licenceInfo}`;
+      $template += `## Application Image\n\n ![Image of Application](${response.image})`;
+      fs.writeFile(path, $template, function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+
+}
